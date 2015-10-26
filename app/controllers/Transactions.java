2ae4a16fea43b98b5014b0ab01586b41fa98bd32
randomlocks <Gokuandgohan1>
2015-10-27 05:56:51 +0800

    tt

---
+++ app/controllers/Transactions.java
@@ -0,0 +1,58 @@
+package controllers;
+
+/**
+ * Created by abdul on 10/21/15.
+ */
+
+import com.avaje.ebean.PagedList;
+import com.typesafe.config.ConfigException;
+import models.Transaction;
+import play.data.Form;
+import play.mvc.Result;
+import play.mvc.Controller;
+import views.html.User.Transaction.newtransaction;
+import views.html.index;
+
+import java.util.Date;
+
+
+public class Transactions extends Controller{
+
+    public static int fare=0;
+    public static Date date;
+
+
+    Form<Transaction> transactionForm = Form.form(Transaction.class);
+
+
+    public Result list(int page) {
+        PagedList<Transaction> list = Transaction.findPage(page, 10);
+        //return ok(list.render);
+        return TODO;
+    }
+
+    public Result newTransaction() {
+
+        return ok(newtransaction.render(transactionForm));
+
+    }
+
+    public Result details(int id) {
+        return TODO;
+    }
+
+    public Result save() {
+
+        Form<Transaction> form = transactionForm.bindFromRequest();
+        Transaction transaction = form.get();
+        transaction.date=date;
+        transaction.amount=fare;
+        return ok(index.render(transaction.mode)); //needs updation
+
+
+    }
+
+    public Result delete(int id) {
+        return TODO;
+    }
+}
